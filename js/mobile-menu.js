136649c3e03449fe096a424f7454c8f6227fba75
Andriy Yarmosh <101985044+HeadOd@users.noreply.github.com>
2023-01-11 18:07:29 +0100

    full mobile

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,21 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+export const refs = {
+  headerList: document.querySelector('.header__list'),
+  headerMenu: document.querySelector('.header__menu'),
+  menuBtnRef: document.querySelector("[data-menu-button]"),
+  mobileMenuRef: document.querySelector("[data-menu]"),
+}
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+refs.headerList.addEventListener('click', headerMenuBar);
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+refs.menuBtnRef.addEventListener("click", headerMenuBar);
 
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+
+function headerMenuBar() {
+  const expanded =
+    refs.menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+  refs.menuBtnRef.classList.toggle("is-open");
+  refs.menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+  refs.mobileMenuRef.classList.toggle("is-open");
+}
